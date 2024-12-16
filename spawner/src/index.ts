8cffe056797421f51671296cf719cda9c2723629
harkirat <harkirat96@gmail.com>
2024-12-17 00:43:07 +0530

    Boilerplate

--- spawner/src/index.ts
+++ spawner/src/index.ts
@@ -1,14 +1,10 @@
-import { Builder, Browser, By, Key, until } from 'selenium-webdriver'
+import { Builder, Browser, By, until, WebDriver } from 'selenium-webdriver'
 import { Options } from 'selenium-webdriver/chrome'
 
-async function main() {
-  const options = new Options({})
-  options.addArguments("--disable-blink-features=AutomationControlled");
-  options.addArguments("--use-fake-ui-for-media-stream");
-  let driver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build()
+async function openMeet(driver: WebDriver) {
   
   try {
-    await driver.get('https://meet.google.com/qtz-ntkc-wft');
+    await driver.get('https://meet.google.com/xor-zvhr-uif');
     ​​const popupButton = await driver.wait(until.elementLocated(By.xpath('//span[contains(text(), "Got it")]')), 10000);
     await popupButton.click();
     ​​const nameInput = await driver.wait(until.elementLocated(By.xpath('//input[@placeholder="Your name"]')), 10000);
@@ -19,9 +15,28 @@ async function main() {
     ​​const buttonInput = await driver.wait(until.elementLocated(By.xpath('//span[contains(text(), "Ask to join")]')), 10000);
     buttonInput.click();
     await driver.wait(until.elementLocated(By.id('123123c12')), 1000000);
+    
   } finally {
     await driver.quit()
   }
 }
 
+async function getDriver() {
+    const options = new Options({})
+    options.addArguments("--disable-blink-features=AutomationControlled");
+    options.addArguments("--use-fake-ui-for-media-stream");
+    let driver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build()
+    return driver;
+}
+
+function startScreenshare(driver: WebDriver) {
+
+}
+
+async function main() {
+    const driver = await getDriver();
+    await openMeet(driver);
+    // wait until admin lets u join
+    await startScreenshare(driver);
+}
 main();
